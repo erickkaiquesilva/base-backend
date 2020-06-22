b7cd0f58edfcfaff20496420020a0ab385fe23d4
kd_o_guh <gugafsdb@gmail.com>
2020-06-22 18:39:45 -0300

    email de novo e de novo

--- base-microservices/email/src/main/java/com/greencode/email/controller/EmailController.java
+++ base-microservices/email/src/main/java/com/greencode/email/controller/EmailController.java
@@ -7,6 +7,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
 import java.util.Random;
 
 import com.greencode.email.dao.UsuariosRepository;
