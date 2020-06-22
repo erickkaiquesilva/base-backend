cdcd578511f1e27b337e1bd923d3f4eff6acc0d9
kd_o_guh <gugafsdb@gmail.com>
2020-06-22 19:07:27 -0300

    email mais uma vez :(

--- base-microservices/email/src/main/java/com/greencode/email/controller/EmailController.java
+++ base-microservices/email/src/main/java/com/greencode/email/controller/EmailController.java
@@ -11,6 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Properties;
 import java.util.Random;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import com.greencode.email.dao.UsuariosRepository;
 import com.greencode.email.gerarsenha.GerarSenha;
 import com.greencode.email.model.Usuario;
@@ -31,6 +40,25 @@ public class EmailController {
 	@PostMapping("/email/enviar")
 	public ResponseEntity<String> enviarNovaSenha(@RequestBody String email){
 		
+		//mexi aqui
+		
+        String to = email;
+        String from = "dont.reply.greencode@gmail.com";
+        Properties properties = System.getProperties();
+        
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(from, "Green123Code");
+
+            }
+
+        });
+		
+		//pare as variaveis aqui
+		
+		
 		Random gerador = new Random();
 		Usuario usuario = tds.buscarEmail(email);
 		GerarSenha gs = new GerarSenha();
@@ -41,16 +69,44 @@ public class EmailController {
 		
 		
 		if(usuario != null) {
+			
 			String senha = gs.exibeSenha(gerador.nextBoolean());
 			tds.atualizarSenha(senha, usuario.getId());
 			
-			SimpleMailMessage msg = new SimpleMailMessage();
-	        msg.setTo(email);
-	
-	        msg.setSubject("Sua Nova Senha");
-	        msg.setText("Nova Senha: " + senha);
-	
-	        javaMailSender.send(msg);
+			  try {
+		            // Create a default MimeMessage object.
+		            MimeMessage message = new MimeMessage(session);
+
+		            // Set From: header field of the header.
+		            message.setFrom(new InternetAddress(from));
+
+		            // Set To: header field of the header.
+		            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+		            // Set Subject: header field
+		            message.setSubject("Sua nova senha de acesso da Green Code");
+
+		            // Now set the actual message
+		            message.setText(senha);
+
+		            System.out.println("enviando...");
+		            // Send message
+		            Transport.send(message);
+		            System.out.println("Mensagem enviada com exito....");
+		        } catch (MessagingException mex) {
+		            mex.printStackTrace();
+		        }
+			
+//			String senha = gs.exibeSenha(gerador.nextBoolean());
+//			tds.atualizarSenha(senha, usuario.getId());
+//			
+//			SimpleMailMessage msg = new SimpleMailMessage();
+//	        msg.setTo(email);
+//	
+//	        msg.setSubject("Sua Nova Senha");
+//	        msg.setText("Nova Senha: " + senha);
+//	
+//	        javaMailSender.send(msg);
 			
 			return ResponseEntity.ok("Senha Alterada, Verifique seu Email");
 		}
